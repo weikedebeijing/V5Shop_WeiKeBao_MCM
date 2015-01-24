402fdc87ca6c28b513fbcbd3fac10f8e643d896d
bing <bx153410@icloud.com>
2015-01-24 14:29:32 +0800

    2015/1/24update

--- Scripts/DrpSetting/paymentJs.js
+++ Scripts/DrpSetting/paymentJs.js
@@ -3,11 +3,57 @@
 
 $(function () {
     $("#AddBatch").click(function () {
-        popupOperate("gray-add-payment", "支付方式设置", "gray-add-payment");
-
+        //popupOperate("gray-add-payment", "支付方式设置", "gray-add-payment");
+        $("#Account").val("");
+        $("#PID").val("");
+        $("#Key").val("");
+        Clear();
+        $("#myModal").modal("show");
     });
 
+    $("#addBtn").click(function () {
+        var validata = AjaxFormValidata();
+        if (validata) {
+            btnsubmit();
+        }
+        else {
+            return validata;
+        }
+    });
+
+    $("#editBtn").click(function () {
+        var validata = EditAjaxFormValidata();
+        if (validata) {
+            btn();
+        }
+        else {
+            return validata;
+        }
+    });
 });
+function Clear() {
+    $("#AlipayType").attr("disabled", false);
+    $("#AlipayTypeError").removeClass("error");
+    $("#AccountError").removeClass("error");
+    $("#PIDError").removeClass("error");
+    $("#KeyError").removeClass("error");
+    $("#AliPayTypePrompt").css("display", "none");
+    $("#AccountPrompt").css("display", "none");
+    $("#PIDPrompt").css("display", "none");
+    $("#KeyPrompt").css("display", "none");
+}
+
+function editClear() {
+    $("#EditAlipayType").attr("disabled", false);
+    $("#EditAlipayTypeError").removeClass("error");
+    $("#EditAccountError").removeClass("error");
+    $("#EditPIDError").removeClass("error");
+    $("#EditKeyError").removeClass("error");
+    $("#EditAliPayTypePrompt").css("display", "none");
+    $("#EditAccountPrompt").css("display", "none");
+    $("#EditPIDPrompt").css("display", "none");
+    $("#EditKeyPrompt").css("display", "none");
+}
 function paymentClose() {
 
     popupClose('gray-add-payment');
@@ -15,37 +61,33 @@ function paymentClose() {
 
 // ajax.BeginForm 的提交前验证
 function AjaxFormValidata() {
-
-    $(".prompt").text("");
-
-
     if ($("#Account").val() == "") {
-        $(".prompt", $("#Account").parent().parent()).text("支付账户不能为空");
-        $("#Account").focus();
+        validataPrompts("Account", "支付账户不能为空");
         return false;
     }
     if (!IsPositiveNum($("#PID"))) {
 
-        $(".prompt", $("#PID").parent().parent()).text("合作者身份(PID)格式不对");
-        $("#PID").focus();
+        validataPrompts("PID", "合作者身份(PID)格式不对");
         return false;
     }
     if ($("#Key").val() == "") {
-        $(".prompt", $("#Key").parent().parent()).text("安全校验码(Key)不能为空");
-        $("#Key").focus();
+        validataPrompts("Key", "安全校验码(Key)不能为空");
         return false;
     }
-
+    return true;
 
 }
-
+var validataPrompts = function (element, msg) {
+    $("#" + element + "Prompt").text(msg);
+    $("#" + element + "Prompt").css("display", "block");
+    $("#" + element + "Error").addClass("error");
+};
 function afterOnSuccess() {
 
     if ($("#UpdateTargetHiddenID").html() == "ok") {
-        $(".Loading").removeClass("style03 style02").addClass("style01");
-        $("#operateTip").html("添加成功！").change();
+        Show("添加成功", "success");
         window.location.reload();
-       
+
         //                  // 把添加成功放到<span>中
         //                  $("#ResultTip").css("display", "block").text("添加支付方式成功");
 
@@ -53,8 +95,7 @@ function afterOnSuccess() {
         //                   $("#ResultTip").css("display", "block").text($("#UpdateTargetHiddenID").html());
     } else {
 
-        $(".Loading").removeClass("style01 style02").addClass("style03"); //红色
-        $("#operateTip").html("" + $("#UpdateTargetHiddenID").html() + "").change();
+        Show("" + $("#UpdateTargetHiddenID").html() + "", "error");
 
     }
 
@@ -63,9 +104,7 @@ function afterOnSuccess() {
 // 添加会员等级失败
 function afterOnFailure() {
     // 把添加失败放到<span>中
-    $(".Loading").removeClass("style01 style02").addClass("style03"); //红色
-
-    $("#operateTip").html("添加支付方式失败").change();
+    Show("添加支付方式失败", "error");
 };
 
 function btnsubmit() {
@@ -80,13 +119,12 @@ function btnsubmit() {
 
 
 function editPayment(paymentId) {
+    editClear();
     // 默认把操作结果隐藏
     $("#ResultTip").css("display", "none");
     $("#EditAlipayType").val($("#DllName").val());
     $("#EditAlipayType").attr("disabled", "disabled");
     $.getJSON("../../DRPSetting/EditPayment", { "paymentId": paymentId }, function (jsonData) {
-
-
         if (jsonData != "") {
 
             $("#id").val(jsonData.ID);
@@ -95,38 +133,30 @@ function editPayment(paymentId) {
             $("#EditPID").val(jsonData.QueryArray.split(',')[1]);
             $("#EditKey").val(jsonData.QueryArray.split(',')[2]);
         }
-
-
-
     });
-    popupOperate("gray-edit-prize", "支付方式设置", "gray-edit-prize");
+
+    //popupOperate("gray-edit-prize", "支付方式设置", "gray-edit-prize");
+    $("#editModal").modal("show");
 }
 
 
 
 // ajax.BeginForm 的提交前验证
 function EditAjaxFormValidata() {
-
-    $(".prompt").text("");
-
-
     if ($("#EditAccount").val() == "") {
-        $(".prompt", $("#EditAccount").parent().parent()).text("支付宝账户不能为空");
-        $("#EditAccount").focus();
+        validataPrompts("EditAccount", "支付宝账户不能为空");
         return false;
     }
     if (!IsPositiveNum($("#EditPID"))) {
 
-        $(".prompt", $("#EditPID").parent().parent()).text("合作者身份(PID)格式不对");
-        $("#EditPID").focus();
+        validataPrompts("EditPID", "合作者身份(PID)格式不对");
         return false;
     }
     if ($("#EditKey").val() == "") {
-        $(".prompt", $("#EditKey").parent().parent()).text("安全校验码(Key)不能为空");
-        $("#EditKey").focus();
-
+        validataPrompts("EditKey", "安全校验码(Key)不能为空");
         return false;
     }
+    return true;
 
 
 }
@@ -134,14 +164,12 @@ function EditAjaxFormValidata() {
 function EditafterOnSuccess() {
     if ($("#EditUpdateTargetHiddenID").text() == "ok") {
         // 把添加成功放到<span>中
-        $(".Loading").removeClass("style03 style02").addClass("style01");
-        $("#operateTip").html("编辑成功！").change();
+        Show("编辑成功", "success");
         window.location.reload();
-     
+
 
     } else {
-        $(".Loading").removeClass("style01 style02").addClass("style03"); //红色
-        $("#operateTip").html($("#EditUpdateTargetHiddenID").val()).change();
+        Show($("#EditUpdateTargetHiddenID").val(), "error");
     }
 
 }
@@ -149,8 +177,7 @@ function EditafterOnSuccess() {
 // 添加会员等级失败
 function EditafterOnFailure() {
     // 把添加失败放到<span>中
-    $(".Loading").removeClass("style01 style02").addClass("style03"); //红色
-    $("#operateTip").html("编辑支付方式失败").change();
+    Show("编辑支付方式失败", "error");
 }
 function editpaymentClose() {
 
